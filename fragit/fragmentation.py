f6dc286337eebfda9623cba466000493a8280d06
mschlinsog <meganschlinsog@gmail.com>
2025-08-08 13:58:45 -0400

    status from mo

--- fragit/fragmentation.py
+++ fragit/fragmentation.py
@@ -584,7 +584,8 @@ class Fragmentation(FragItConfig):
         if len(atoms) == 1:
             atom = self.mol.GetAtom(atoms[0])
             charge_lbl = charge_lbls[atom.GetFormalCharge()]
-            element = Z2LABEL[atom.GetAtomicNum()]
+#            element = Z2LABEL[atom.GetAtomicNum()]
+            element = LABEL2Z[atom.GetAtomicNum()]
             return "{0:s}{1:s}".format(element, charge_lbl)
         else:
             for residue in openbabel.OBResidueIter(self.mol):
